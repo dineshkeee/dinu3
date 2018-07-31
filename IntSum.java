22ed63c80b868d950344e3521222791a060307bf
dineshkeee <41099282+dineshkeee@users.noreply.github.com>
2018-07-31 21:24:20 +0530

    Add files via upload

---
+++ IntSum.java
@@ -0,0 +1,10 @@
+package set4;
+import java.util.*;
+public class IntSum {
+	public static void main(String args[]){
+		Scanner in=new Scanner(System.in);
+		int a=in.nextInt();
+		int b=in.nextInt();
+		System.out.println(a+b);
+	}
+}
