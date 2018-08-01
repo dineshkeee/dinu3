a34878c8c73925ca6c029a55107d35d65def3fd5
dineshkeee <41099282+dineshkeee@users.noreply.github.com>
2018-08-01 17:44:34 +0530

    Add files via upload

---
+++ append.java
@@ -0,0 +1,9 @@
+package set4;
+import java.util.*;
+public class append {
+	public static void main(String args[]){
+		Scanner in=new Scanner(System.in);
+		String a=in.nextLine();
+		System.out.println(a+".");
+	}
+}
