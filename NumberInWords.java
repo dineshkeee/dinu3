6c92d7809d49ed6b558b6041917dd5736faba7fe
dineshkeee <41099282+dineshkeee@users.noreply.github.com>
2018-08-07 10:16:07 +0530

    Add files via upload

---
+++ NumberInWords.java
@@ -0,0 +1,31 @@
+package set4;
+import java.util.*;
+public class NumberInWords {
+	public static void main(String args[]){
+		String arr[]={"one","two","three","four","five","six","seven","eight","nine","ten"};
+		Scanner in=new Scanner(System.in);
+		String a=in.next();
+		switch(a){
+		case "1":
+			System.out.println(arr[0]);break;
+		case "2":
+			System.out.println(arr[1]);break;
+		case "3":
+			System.out.println(arr[2]);break;
+		case "4":
+			System.out.println(arr[3]);break;
+		case "5":
+			System.out.println(arr[4]);break;
+		case "6":
+			System.out.println(arr[5]);break;
+		case "7":
+			System.out.println(arr[6]);break;
+		case "8":
+			System.out.println(arr[7]);break;
+		case "9":
+			System.out.println(arr[8]);break;
+		case "10":
+			System.out.println(arr[9]);break;
+	}
+	}
+}
