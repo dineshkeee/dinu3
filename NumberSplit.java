5eb26a63e150d4c3b27740d3cde9b3f556f484c1
dineshkeee <41099282+dineshkeee@users.noreply.github.com>
2018-08-07 09:27:13 +0530

    Add files via upload

---
+++ NumberSplit.java
@@ -0,0 +1,13 @@
+package set4;
+import java.util.*;
+public class NumberSplit {
+		public static void main(String args[]){
+			Scanner in=new Scanner(System.in);
+			String a=in.next();
+			for(int i=0;i<a.length();i++){
+				System.out.print(a.substring(i,i+1)+" ");
+			}
+			
+		}
+
+}
