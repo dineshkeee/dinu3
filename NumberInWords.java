006e3cec41ea75c4db3de331207cc461f277589f
dineshkeee <41099282+dineshkeee@users.noreply.github.com>
2018-08-07 10:21:23 +0530

    Update NumberInWords.java

--- NumberInWords.java
+++ NumberInWords.java
@@ -2,30 +2,30 @@ package set4;
 import java.util.*;
 public class NumberInWords {
 	public static void main(String args[]){
-		String arr[]={"one","two","three","four","five","six","seven","eight","nine","ten"};
 		Scanner in=new Scanner(System.in);
 		String a=in.next();
 		switch(a){
 		case "1":
-			System.out.println(arr[0]);break;
+			System.out.println("one");break;
 		case "2":
-			System.out.println(arr[1]);break;
+			System.out.println("two");break;
 		case "3":
-			System.out.println(arr[2]);break;
+			System.out.println("three");break;
 		case "4":
-			System.out.println(arr[3]);break;
+			System.out.println("four");break;
 		case "5":
-			System.out.println(arr[4]);break;
+			System.out.println("five");break;
 		case "6":
-			System.out.println(arr[5]);break;
+			System.out.println("six");break;
 		case "7":
-			System.out.println(arr[6]);break;
+			System.out.println("seven");break;
 		case "8":
-			System.out.println(arr[7]);break;
+			System.out.println("eight");break;
 		case "9":
-			System.out.println(arr[8]);break;
+			System.out.println("nine");break;
 		case "10":
-			System.out.println(arr[9]);break;
+			System.out.println("ten");break;
 	}
 	}
 }
+
