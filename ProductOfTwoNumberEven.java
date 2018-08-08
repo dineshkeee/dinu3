6cf54937bae06e667cdd41006cff01b9195eee3e
dineshkeee <41099282+dineshkeee@users.noreply.github.com>
2018-08-08 11:09:22 +0530

    Create ProductOfTwoNumberEven.java

---
+++ ProductOfTwoNumberEven.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class ProductOfTwoNumberEven{
+public static void main(String args[]){
+Scanner in=new Scanner(System.in);
+int a=in.nextInt();
+int b=in.nextInt();
+int c=a*b;
+if(c%2==0){
+System.out.println("Even");
+}else{
+System.out.println("Odd");}}}
