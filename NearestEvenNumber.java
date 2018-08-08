4303a6d74fa34a0230ad7f91ea84ae62d6c83c0b
dineshkeee <41099282+dineshkeee@users.noreply.github.com>
2018-08-08 10:58:25 +0530

    Create NearestEvenNumber.java

---
+++ NearestEvenNumber.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class NearestEvenNumber{
+public static void main(String args[]){
+Scanner in=new Scanner(System.in);
+int a=in.nextInt();
+if(a%2==0){
+System.out.println(a);
+}else{
+System.out.println(a-1);}}}
