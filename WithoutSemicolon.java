cfbfb5dfd7b9fdc09c4c92ec3e0f0dc782fe9119
dineshkeee <41099282+dineshkeee@users.noreply.github.com>
2018-08-22 11:38:06 +0530

    Update WithoutSemicolon.java

--- WithoutSemicolon.java
+++ WithoutSemicolon.java
@@ -1,5 +1,8 @@
 package guvi;
-
 public class WithoutSemicolon {
+  public static void main(String args[]){
+	 Scanner in=new Scanner(System.in);
+	 String a=in.nextLine();
+     if (System.out.append(a).equals(null)) {}
 
 }
