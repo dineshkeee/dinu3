44d51de6ba55e38b9dfbc95b788208c2351f9408
dineshkeee <41099282+dineshkeee@users.noreply.github.com>
2018-08-22 10:27:51 +0530

    Add files via upload

---
+++ IntLength.java
@@ -0,0 +1,10 @@
+package guvi;
+import java.util.*;
+public class IntLength {
+	public static void main(String args[]){
+		Scanner in=new Scanner(System.in);
+		int a=in.nextInt();
+	 System.out.println(String.valueOf(a).length());
+	}
+
+}
