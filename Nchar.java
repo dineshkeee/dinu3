9c0799ee45f902966370cbc92d51c9b88042864d
dineshkeee <41099282+dineshkeee@users.noreply.github.com>
2018-08-22 10:38:32 +0530

    Add files via upload

---
+++ Nchar.java
@@ -0,0 +1,10 @@
+package guvi;
+import java.util.*;
+public class Nchar {
+	 public static void main(String args[]){
+		 Scanner in=new Scanner(System.in);
+		 String a=in.next();
+		 int l=in.nextInt();
+		 System.out.println(a.substring(0,l));
+	 }
+}
